0292f7fd259097240be3898b1242f99b3e73df94
Sinus <peoplebj@naver.com>
2020-07-21 16:29:55 +0900

    [Add] Get a list of site from local txt file & get a screenshot after visiting a webaite

--- main.js
+++ main.js
@@ -2,27 +2,83 @@ const fs = require("fs");
 const crypto = require("crypto");
 const puppeteer = require("puppeteer");
 
-const directory = "./wasm/";
+const wdirectory = "./Wasm/";
+const fdirectory = "./FailList/";
+const sdirectory = "./Screenshot/";
 const wrapper = fs.readFileSync("./wrapper.js", "utf8");
 
-function wasmFound(data) {
-    //Use hash as filename for deduplication
+const sitelist = fs.readFileSync("site4000.txt");
+lineArray = sitelist.toString().split('\r\n');
+
+function wasmFound(data, slink) {
+    // Use hash as filename for deduplication
     const filename = crypto.createHash("md5").update(data).digest("hex");
-    fs.writeFileSync(directory + filename, Buffer.from(data, "base64"));
+    fs.writeFileSync(wdirectory + filename, Buffer.from(data, "base64"));
+	console.log("WASM Found!");
+	// Record the information about the site, and the module has been discovered
+	fs.appendFile(wdirectory + "WasmSiteList.txt", '\ufeff' + slink + ' ' + filename + '\r\n', function (err) {
+		if (err) {
+			throw err;
+		}
+	});
 }
 
 (async () => {
-    if (!fs.existsSync(directory)){
-        fs.mkdirSync(directory);
+    if (!fs.existsSync(wdirectory)){
+        fs.mkdirSync(wdirectory);
     }
+	
+	if (!fs.existsSync(fdirectory)){
+        fs.mkdirSync(fdirectory);
+    }
+	
+	if (!fs.existsSync(sdirectory)){
+        fs.mkdirSync(sdirectory);
+    }
+	
+	var prot = "http://"
+	
+	for (var i = 197; i < 199; i++) {
+		const browser = await puppeteer.launch();
+		try {
+			var slink = lineArray[i];
+			
+			const page = await browser.newPage();
+			await page.exposeFunction("wasmFound", source => wasmFound(source, slink));
+			await page.evaluateOnNewDocument(wrapper);
+			
+			// Add http:// in front of URL, cuz puppeteer generates error otherwise	
+			await page.goto(prot + slink);
+			
+			// Wait a bit (30 seconds), to make sure the Wasm as chance to load
+			await new Promise(done => setTimeout(done, 30000));
+			
+			// Take a screenshot of the page
+			var ppath = sdirectory + 'Screenshot' + i + '.png';
+			await page.screenshot({path: ppath});
+			
+			// Close the page and browser instance
+			await page.close();
+			await browser.close();
+		}
+		catch (e) {
+			console.log(e.message);
+			// Record the information about the site, and generated error
+			fs.appendFile(fdirectory + "FailSiteList.txt", '\ufeff' + slink + '\r\n', function (err) {
+				if (err) {
+					throw err;
+				}
+			});
+			await browser.close();
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.exposeFunction("wasmFound", source => wasmFound(source));
-    await page.evaluateOnNewDocument(wrapper);
-    await page.goto("https://magnum.graphics/showcase/picking/")
-
-    //Wait a bit, to make sure the Wasm as chance to load
-    await new Promise(done => setTimeout(done, 1000));
-    await browser.close();
+		}
+		finally {
+			
+			// Show current progress on the console
+			console.log(i, slink);
+			await browser.close();
+		}
+	}
+	
+	
 })();
